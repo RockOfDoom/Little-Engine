69c71e12fececc3446b8bc0f5b5970a492287929
Alex Basinski <abasinsk@ucsc.edu>
2021-04-23 15:45:54 -0700

    in-progress jumping

--- src/prefabs/Engine.js
+++ src/prefabs/Engine.js
@@ -1,7 +1,12 @@
-class Engine extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
+class Engine extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame, attackKey, jumpKey) {
         super(scene, x, y, texture, frame);
-        scene.add.existing(this);
+        scene.add.existing(this); //add sprite to scene
+        scene.physics.add.existing(this); //assign physics to sprite
+        //load input keys
+        this.aKey = attackKey;
+        this.jKey = jumpKey;
+        //define variables that track the current action the engine is performing
         this.running = true;
         this.attacking = false;
         this.jumping = false;
@@ -10,9 +15,27 @@ class Engine extends Phaser.GameObjects.Sprite {
         this.hurting = false;
     }
 
-    update() {}
+    update(speed, gas, frame) {
+        this.x = borderUISize + 5 * speed;
 
-    jump() {}
+        if(this.jKey.isDown && !this.airborne && !this.attacking) {
+            console.log("jump initiated");
+            this.jump();
+        }
+    }
+
+    jump() {
+        this.jumping = true;
+        this.airborne = true;
+        this.setVelocity(0, -100);
+        console.log("jump executed");
+    }
+
+    land() {
+        this.landing = true;
+        this.airborne = false;
+        console.log(this.airborne);
+    }
 
     attack() {}
 }
